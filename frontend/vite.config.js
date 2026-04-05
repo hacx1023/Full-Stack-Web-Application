3bb0a43bc0140590a128440827a8f4b5cb0b8bf6
Shubham Kumar <69086696+hacx1023@users.noreply.github.com>
2026-04-05 19:37:41 +0530

    Remove proxy settings from Vite config
    Removed proxy configuration from Vite server settings.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  
   server: {
-    host: "0.0.0.0",   // ✅ VERY IMPORTANT
-    port: 5173,        // (optional but good)
-    proxy: {
-      '/api': 'http://backend-cont:5000',     // ✅ use container name
-      '/uploads': 'http://backend-cont:5000'
-    }
+    host: "0.0.0.0",
+    port: 5173
   }
 })
